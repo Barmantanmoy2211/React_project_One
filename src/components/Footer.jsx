c638ea8f3f2a86ba6e64cc759d18cfdc0d705a77
Tanmoy Barman <barmantanmoy59@gmail.com>
2023-02-06 20:08:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+
+        <h1>ByteCoders</h1>
+        <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://youtube.com" target={"blank"}>Youtube</a>
+                <a href="https://github.com/Barmantanmoy2211" target={"blank"}>Github</a>
+                <a href="https://www.instagram.com/barmantanmoy59/" target={"blank"}>Instagram</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
